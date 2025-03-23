234f1876254a7fe6d4a8e75eb777cb0a18420a78
d-ruskovsky <xromand@stuba.sk>
2025-03-23 23:01:18 +0100

    v04, pridaná funkcia elemfun_n, mierna úprava triple_dif (*args pre stupeň polynómu), úprava fukushima.py pre výpočet potencialu V (súčet od 0 po 1)

--- function/fukushima.py
+++ function/fukushima.py
@@ -7,17 +7,9 @@ import math             # na binomický koeficient math.comb()
 
 # TO DO : 
 #
-# Začať riešiť POTENCIAL, výpočet zatiaľ riešiť pre homogénnu prizmu (n=0), 
-# výpočet aj tak musí začať homogénnou prizmou, až potom riešiť výpočet cez 
-# cyklus pre rho = 2+. 
-#
-# Vo vzorci (13) vstupuje súradnica z výpočtového bodu, vo vzorci (14) je táto
-# súradnica umocnená na index j ktorý = n - m
-#
-# OTÁZNE - index j netreba riešiť, stačí ho nahradiť n - m. N je stupeň 
-# polynómu rho, ??? m nadobúda hodnoty 0-N a tiež n ??? 
-#
-# Bolo by dobré túto funkciu prepísať do jazyka c, ale to až bude funkčná
+# vyriešiť indexy a opraviť výpočty koeficientov c vo výpočte V,g. zatiaľ sú sumácie dané manuálne 
+# 
+# dopočítať tenzory
 
 def Fukushima(coor_prism,coor_point,rho):
 
@@ -36,33 +28,45 @@ def Fukushima(coor_prism,coor_point,rho):
     X1, Y1, Z1 = coor_prism[0] - coor_point[0], coor_prism[2] - coor_point[1], coor_prism[4] - coor_point[2]                # (10) posunutie "shifted endpoints"
     X2, Y2, Z2 = coor_prism[1] - coor_point[0], coor_prism[3] - coor_point[1], coor_prism[5] - coor_point[2] 
     
+    N = len(rho)        # stupeň polynómu
+
     # ---------- Homogénna prizma ------------- # 
 
     # Potenciál - V ------
-    U_0 = lambda X, Y, Z, func_val: \
-        -((X**2 * func_val[0]) + (Y**2 * func_val[1]) + (Z**2 * func_val[2]) / 2) + \
-              (Y * Z * func_val[3]) + (Z * X * func_val[4]) + (X * Y * func_val[5])                                         # (23) ako anonýmna funkcia lambda
+    U_0 = lambda X, Y, Z, func_val_0: \
+        -((X**2 * func_val_0[0]) + (Y**2 * func_val_0[1]) + (Z**2 * func_val_0[2]) / 2) + \
+              (Y * Z * func_val_0[3]) + (Z * X * func_val_0[4]) + (X * Y * func_val_0[5])                                   # (23) ako anonýmna funkcia lambda
+    U_1 = lambda X, Y, Z, func_val_1: \
+        -((Z**(1+2) * func_val_1[5]) / (1 + 2)) + ((Z**(1+1) * ((Y * func_val_1[2]) + (X * func_val_1[1]))) / (1 + 1)) \
+        - (((Y * func_val_1[4]) + (X * func_val_1[3])) / ((1 + 1) * (1 + 2)))                                               # (26)
 
     W_0 = triple_dif(U_0,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)                                                                 # (9)
+    W_1 = triple_dif(U_1,X1,Y1,Z1,X2,Y2,Z2,N,homogenous=False)
 
-    # Indexy !! prekontrolovať tieto vzorce až budú indexy vyriešené
-    c_0j = 1 * G * rho[0]                                                                                                   # (15) pri n=0,  binom. koef = 1 
+    # V tomto bode je výpočet samotných hodnôt potenciálu a zrýchlenia veľmi pochybný pretože si nie som istý indexami,
+    # z tohto titulu je výpočet neistý, všetky parametre a spôsob výpočtu parametrov c treba prerobiť (aj pre zrýchlenie g)
+    # až budú indexy isté 
 
-    c_0 = c_0j * (coor_point[2]**1)                                                                                         # (14)
+    c_00 = math.comb(0,0) * G * rho[0]                                                                                      # (15)
+    c_01 = math.comb(1,0) * G * rho[0]
+    c_10 = math.comb(0,1) * G * rho[0]
+    c_11 = math.comb(1,1) * G * rho[0]
 
-    # Počiatočná hodnota potenciálu ----
-    V = c_0 * W_0                                                                                                           # (13)
+    c_0 = (c_00 * (coor_point[2]**0)) + (c_01 * (coor_point[2]**1))                                                         # (14)
+    c_1 = (c_10 * (coor_point[2]**0)) + (c_11 * (coor_point[2]**1))
+
+    V = (c_0 * W_0) + (c_1 * W_1)                                                                                           # (13)
 
     # Zrýchlenie - g ------ 
 
-    U_0x = lambda X, Y, Z, func_val: \
-            -(X * func_val[0]) + (Y * func_val[5]) + (Z * func_val[4])                                                      # (23)
+    U_0x = lambda X, Y, Z, func_val_0: \
+            -(X * func_val_0[0]) + (Y * func_val_0[5]) + (Z * func_val_0[4])                                                # (23)
 
-    U_0y = lambda X, Y, Z, func_val: \
-            (X * func_val[5]) - (Y * func_val[1]) + (Z * func_val[3])
+    U_0y = lambda X, Y, Z, func_val_0: \
+            (X * func_val_0[5]) - (Y * func_val_0[1]) + (Z * func_val_0[3])
     
-    U_0z = lambda X, Y, Z, func_val: \
-            (X * func_val[4]) + (Y * func_val[3]) - (Z * func_val[2])
+    U_0z = lambda X, Y, Z, func_val_0: \
+            (X * func_val_0[4]) + (Y * func_val_0[3]) - (Z * func_val_0[2])
     
     dW_0x = -(triple_dif(U_0x,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))                                                           # (20)
     dW_0y = -(triple_dif(U_0y,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))
@@ -72,7 +76,7 @@ def Fukushima(coor_prism,coor_point,rho):
     # zatiaľ uvažujem že n = 1, m = 0 - ale úprimne neviem úplne prečo, just a hunch, aj pri potenciáli to mám inak :) 
     d_c0_nm = 1 * math.comb(1 + 0 + 1,1) * G * rho[0]                                                                       # (19)
 
-    # Opäť som odignoroval sumáciu kvôli indexom, zároveň indexy :) 
+    # Opäť som odignoroval sumáciu kvôli indexom, zároveň indexy samotné :) 
     d_c_0 = d_c0_nm * coor_point[2]**1
 
     # Počiatočná hodnota zrýchlenia ----
@@ -81,13 +85,18 @@ def Fukushima(coor_prism,coor_point,rho):
     g_y = c_0 * dW_0y
     g_z = (c_0 * dW_0z) + (d_c_0 * W_0)
 
+    # Tenzor - T ------
+
+
+    
+
     # ---------- Nehomogénna prizma ------------- # 
 
     # :) 
 
     # -- Posledné úpravy --
 
-    g = [g_x,g_y,g_z]
+    g = [g_x,g_y,g_z]   # vloženie do vektora (tuple)
 
     return V, g
 
--- function/tripledif.py
+++ function/tripledif.py
@@ -3,12 +3,13 @@ import numpy as np
 # Knižnica tripledif 
 
 # Aktuálne obsahuje 3 funkcie - 
-#                               elemfun_0              - elementárne funkcie (homogénna prizma)
-#                               elemfun_n              - elementárne funkcie (nehomogénna prizma)
+#                               elemfun_0              - elementárne funkcie (n = 0)
+#                               elemfun_n              - elementárne funkcie (n = 1,2..)
 #                               triple_dif             - operátor trojitej diferencie (Fukushima (11))
 
 # TO DO: 
-#            urobiť funkciu pre elemfun_n 
+#            skontrolovať správnosť (obzvlášť pri elemfun_n  - rekurentný výpočet)
+#            upraviť triple_dif aby obsahoval argument stupňa polynómu N bez potreby *args
 
 def elemfun_0(X,Y,Z):
 
@@ -17,6 +18,7 @@ def elemfun_0(X,Y,Z):
     # INPUT -   "X,Y,Z"                 - trojrozmerné súradnice bodu posunuté podľa (10) 
 
     # OUTPUT -  "A,B,C,D,E,F"           - elementárne funkcie podľa (24) v prípade homogénnej prizmy  
+    #           "R"                     - euklidovská vzdialenosť
 
     # ============================ F U N K C I A =================================== #
 
@@ -30,26 +32,106 @@ def elemfun_0(X,Y,Z):
     B = np.arctan((Z*X) / (Y*R))
     C = np.arctan((X*Y) / (Z*R))
 
-    return A,B,C,D,E,F
+    return A, B, C, D, E, F, R
+    #      0, 1, 2, 3, 4, 5, 6
+
+def elemfun_n(X,Y,Z,nmax):   
+
+    # funkcia pre výpočet elementárnych funkcií v prípade nehomogénnej prizmy teda n = 1,2.. 
+
+    # INPUT -   "X,Y,Z"                 - trojrozmerné súradnice bodu posunuté podľa (10)
+    #           "nmax"                  - maximálna hodnota rekurentného výpočtu (stupeň polynómu)
+
+    # OUTPUT -  "R_n, E_n, D_n"         - elementárne funkcie vypočítané pre daný stupeň polynómu
+    #           "E_n2, D_n2"            - elementárne funkcie vypočítané pre stupeň polynómu + 2 pre potrebu výpočtu U 
+    #           "C"                     - elementárna funkcia podľa (24) ktorá je taktiež potrebná pre výpočet U ak n=1,2.. 
+
+    # ============================ F U N K C I A =================================== #
+
+    R = elemfun_0(X,Y,Z)[6]         # uloženie premenných z funkcie elemfun_0 pre výpočet v tejto funkcii 
+    F = elemfun_0(X,Y,Z)[5]
+    E = elemfun_0(X,Y,Z)[4]
+    D = elemfun_0(X,Y,Z)[3]
+    C = elemfun_0(X,Y,Z)[2]
+    B = elemfun_0(X,Y,Z)[1]
+    A = elemfun_0(X,Y,Z)[0]
+
+    S = X**2 + Y**2                                                                 # (31)
+
+    R_1 = R                                                                         # (30)
+    R_2 = ((Z * R) - (S * F))/2
+    
+    D_1 = D                                                                         # (28)
+    D_2 = (Y * B) - (X * F)
+
+    E_1 = E
+    E_2 = (X * A) - (Y * F)
+
+    # Rekurentný výpočet R,D,E =====================================================
+    if nmax == 1:       # v prípade že N = 1 ---------------------------------------
+        
+        D_n2 = -(Y**2 * D_1) - (X * R_1)                                             # (27)
+        E_n2 = -(X**2 * E_1) - (Y * R_1)
+
+        R_n = R_1       # ukladanie premenných pre potrebu výpočtu U
+        D_n = D_1
+        E_n = E_1
 
 
-def elemfun_n(X,Y,Z):   
+    elif nmax == 2:     # v prípade že N = 2 ---------------------------------------
 
-    # funkcia pre výpočet elementárnych funkcií v prípade nehomogénnej prizmy
+        D_n2 = -(Y**2 * D_2) - (X * R_2)                                             # (27)
+        E_n2 = -(X**2 * E_2) - (Y * R_2)
 
-    return 0
+        R_n = R_2
+        D_n = D_2
+        E_n = E_2
+
+    else:               # v prípade že N > 2 ---------------------------------------
+        for n in range(3,nmax + 3):     # range od 3 (0,1,2 máme) po nmax + 3 (+1 pretože python neberie poslednú hodnotu, + 2 pretože potrebuje n+2 na výpočet U)
+            
+            R_n = ((Z**(n-1) * R) - ((n - 1) * S * R_1)) / n                        # (29) 
+            D_n = -(Y**2 * D_1) - (X * R_1)                                         # (27)
+            E_n = -(X**2 * E_1) - (Y * R_1)
+
+            if n == (nmax+1):
+                R_main = R_n
+                D_main = D_n
+                E_main = E_n
+
+            R_1 = R_2       # prepísanie premenných 
+            R_2 = R_n 
+
+            D_1 = D_2
+            D_2 = D_n 
+
+            E_1 = E_2
+            E_2 = E_n 
+        # koniec cyklu for - - - - - 
+    
+        R_n = R_main        # ukladanie premenných pre potrebu výpočtu U
+
+        D_n2 = D_n 
+        D_n = D_main
+
+        E_n2 = E_n 
+        E_n = E_main
+
+    return R_n, E_n, D_n, E_n2, D_n2, C 
+    #      0    1    2    3     4     5
 
 
 
-def triple_dif(equation,X1,Y1,Z1,X2,Y2,Z2,homogenous=True):
+def triple_dif(equation,X1,Y1,Z1,X2,Y2,Z2,*args,homogenous=True):
 
     # funkcia trojitej diferencie aplikuje operátor podľa vzťahu Fukushima (11) 
 
     # INPUT -   "equation"              - rovnica obsahujúca súradnice X,Y,Z zadefinovaná anonýmnou funkciou lambda, viď. DEFINE
     #           "X1,Y1...Z2"            - upravené súradnice, shifted endpoints (10)
+    #           "*args"                 - dodatočné argumenty, pridať N (nmax) stupeň polynómu rho pri použití elemfun_n
     #           "homogenous"            - boolean, True - použijú sa elementárne funkcie pre homogénnnu prizmu, False - nehomogénna prizma
 
-    # OUTPUT -  "vysl"                  - vyčíslená hodnota zvolenej rovnice s použitím operátora trojitej diferencie 
+    # OUTPUT -  "triple_difference"     - vyčíslená hodnota zvolenej rovnice s použitím operátora trojitej diferencie 
 
     # DEFINE -  "equation"              - rovnica musí byť definovaná cez lambda (anonýmnu funkciu), kde parametrami funkcie sú súradnice XYZ a 
     #                                      tuple hodnoty vypočítaných element. funkcií pre danú prizmu v poradí v akom ich funkcia elemfun vracia 
@@ -61,31 +143,31 @@ def triple_dif(equation,X1,Y1,Z1,X2,Y2,Z2,homogenous=True):
     else:
         elemfun = elemfun_n
 
-    f1_element_val = elemfun(X2,Y2,Z2)          # výpočet elementárnych funkcií pre rovnicu
+    f1_element_val = elemfun(X2,Y2,Z2,*args)          # výpočet elementárnych funkcií pre rovnicu
     f1 = equation(X2, Y2, Z2,f1_element_val)    # výpočet rovnice na základe daných súradníc 
 
-    f2_element_val = elemfun(X2,Y2,Z1)
+    f2_element_val = elemfun(X2,Y2,Z1,*args)
     f2 = equation(X2, Y2, Z1,f2_element_val)
 
-    f3_element_val = elemfun(X2,Y1,Z2)
+    f3_element_val = elemfun(X2,Y1,Z2,*args)
     f3 = equation(X2, Y1, Z2,f3_element_val)
 
-    f4_element_val = elemfun(X2,Y1,Z1)
+    f4_element_val = elemfun(X2,Y1,Z1,*args)
     f4 = equation(X2, Y1, Z1,f4_element_val)
 
-    f5_element_val = elemfun(X1,Y2,Z2)
+    f5_element_val = elemfun(X1,Y2,Z2,*args)
     f5 = equation(X1, Y2, Z2,f5_element_val)
 
-    f6_element_val = elemfun(X1,Y2,Z1)
+    f6_element_val = elemfun(X1,Y2,Z1,*args)
     f6 = equation(X1, Y2, Z1,f6_element_val)
 
-    f7_element_val = elemfun(X1,Y1,Z2)
+    f7_element_val = elemfun(X1,Y1,Z2,*args)
     f7 = equation(X1, Y1, Z2,f7_element_val)
 
-    f8_element_val = elemfun(X1,Y1,Z1)
+    f8_element_val = elemfun(X1,Y1,Z1,*args)
     f8 = equation(X1, Y1, Z1,f8_element_val)
 
 
-    vysl = f1 - f2 - f3 + f4 - f5 + f6 + f7 - f8            # (11) -    trojitá diferencia funkcie (operátor)
+    triple_difference = f1 - f2 - f3 + f4 - f5 + f6 + f7 - f8            # (11) -    trojitá diferencia funkcie (operátor)
 
-    return vysl
+    return triple_difference
