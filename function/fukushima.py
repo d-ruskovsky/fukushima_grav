148f76fab75c6b2d0ca321607d3aee95819f6e01
d-ruskovsky <xromand@stuba.sk>
2025-04-05 11:22:15 +0200

    v1.0 spravny vypocet grav. ucinku homog. prizmy

--- function/fukushima.py
+++ function/fukushima.py
@@ -6,10 +6,14 @@ import math             # na binomický koeficient math.comb()
 # zrýchlenia, a tenzoru na základe článku Fukushima 2018. 
 
 # TO DO : 
-#
-# vyriešiť indexy a opraviť výpočty koeficientov c vo výpočte V,g. zatiaľ sú sumácie dané manuálne 
+#       vyriešiť indexy a opraviť výpočty koeficientov c vo výpočte V,g. zatiaľ sú sumácie dané manuálne 
 # 
-# dopočítať tenzory
+#       dopočítať tenzory
+#
+#       prerobiť funkcie z formátu lambda na formát def fx return..
+#
+# POZNÁMKY : 
+#       indexy j používať bežne bez riešenia čo sú, pri konštantnom polynóme je N = 0 ! 
 
 def Fukushima(coor_prism,coor_point,rho):
 
@@ -28,34 +32,34 @@ def Fukushima(coor_prism,coor_point,rho):
     X1, Y1, Z1 = coor_prism[0] - coor_point[0], coor_prism[2] - coor_point[1], coor_prism[4] - coor_point[2]                # (10) posunutie "shifted endpoints"
     X2, Y2, Z2 = coor_prism[1] - coor_point[0], coor_prism[3] - coor_point[1], coor_prism[5] - coor_point[2] 
     
-    N = len(rho)        # stupeň polynómu
+    N = len(rho) - 1       # stupeň polynómu
 
     # ---------- Homogénna prizma ------------- # 
 
+    # Koeficienty polynómu -------
+
+    c_mj = math.comb(0,0) * G * rho[0]                                                                                     # (15)
+    d_c0_nm = (0 + 1) * math.comb((0 + 0 + 1) , 0) * G * rho[0]                                                            # (19)
+    dd_c0_nm = (0 + 2) * (0 + 1) * math.comb((0 + 0 + 2), 0) * G * rho[0]
+
+    c_0 = c_mj * (coor_point[2]**0)                                                                                        # (14)
+    d_c_0 = d_c0_nm * (coor_point[2]**0)
+    dd_c_0 = dd_c0_nm * (coor_point[2]**0)
+
     # Potenciál - V ------
     U_0 = lambda X, Y, Z, func_val_0: \
-        -((X**2 * func_val_0[0]) + (Y**2 * func_val_0[1]) + (Z**2 * func_val_0[2]) / 2) + \
-              (Y * Z * func_val_0[3]) + (Z * X * func_val_0[4]) + (X * Y * func_val_0[5])                                   # (23) ako anonýmna funkcia lambda
-    U_1 = lambda X, Y, Z, func_val_1: \
-        -((Z**(1+2) * func_val_1[5]) / (1 + 2)) + ((Z**(1+1) * ((Y * func_val_1[2]) + (X * func_val_1[1]))) / (1 + 1)) \
-        - (((Y * func_val_1[4]) + (X * func_val_1[3])) / ((1 + 1) * (1 + 2)))                                               # (26)
+       -((X**2 * func_val_0[0]) + (Y**2 * func_val_0[1]) + (Z**2 * func_val_0[2])) / 2 + \
+             (Y * Z * func_val_0[3]) + (Z * X * func_val_0[4]) + (X * Y * func_val_0[5])                                   # (23) ako anonýmna funkcia lambda
+
+    # def U_0(X,Y,Z):
+    #     return -((X**2 * elemfun_0[0] + Y**2 * elemfun_0[1] + Z**2 * elemfun_0[2])/2) + \
+    #             Y * Z * elemfun_0[3] + Z * X * elemfun_0[4] + X * Y * elemfun_0[5]
+
+
 
     W_0 = triple_dif(U_0,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)                                                                 # (9)
-    W_1 = triple_dif(U_1,X1,Y1,Z1,X2,Y2,Z2,N,homogenous=False)
 
-    # V tomto bode je výpočet samotných hodnôt potenciálu a zrýchlenia veľmi pochybný pretože si nie som istý indexami,
-    # z tohto titulu je výpočet neistý, všetky parametre a spôsob výpočtu parametrov c treba prerobiť (aj pre zrýchlenie g)
-    # až budú indexy isté 
-
-    c_00 = math.comb(0,0) * G * rho[0]                                                                                      # (15)
-    c_01 = math.comb(1,0) * G * rho[0]
-    c_10 = math.comb(0,1) * G * rho[0]
-    c_11 = math.comb(1,1) * G * rho[0]
-
-    c_0 = (c_00 * (coor_point[2]**0)) + (c_01 * (coor_point[2]**1))                                                         # (14)
-    c_1 = (c_10 * (coor_point[2]**0)) + (c_11 * (coor_point[2]**1))
-
-    V = (c_0 * W_0) + (c_1 * W_1)                                                                                           # (13)
+    V = (c_0 * W_0)                                                                                                         # (13)
 
     # Zrýchlenie - g ------ 
 
@@ -68,37 +72,69 @@ def Fukushima(coor_prism,coor_point,rho):
     U_0z = lambda X, Y, Z, func_val_0: \
             (X * func_val_0[4]) + (Y * func_val_0[3]) - (Z * func_val_0[2])
     
-    dW_0x = -(triple_dif(U_0x,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))                                                           # (20)
-    dW_0y = -(triple_dif(U_0y,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))
-    dW_0z = -(triple_dif(U_0z,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))
-
-    # Tu je to pravdepodobne chybné pretože stále nerozumiem indexom, prerobiť až sa v nich vyznáš 
-    # zatiaľ uvažujem že n = 1, m = 0 - ale úprimne neviem úplne prečo, just a hunch, aj pri potenciáli to mám inak :) 
-    d_c0_nm = 1 * math.comb(1 + 0 + 1,1) * G * rho[0]                                                                       # (19)
-
-    # Opäť som odignoroval sumáciu kvôli indexom, zároveň indexy samotné :) 
-    d_c_0 = d_c0_nm * coor_point[2]**1
+    W_0X = -(triple_dif(U_0x,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))                                                           # (20)
+    W_0Y = -(triple_dif(U_0y,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))
+    W_0Z = -(triple_dif(U_0z,X1,Y1,Z1,X2,Y2,Z2,homogenous=True))
 
     # Počiatočná hodnota zrýchlenia ----
 
-    g_x = c_0 * dW_0x
-    g_y = c_0 * dW_0y
-    g_z = (c_0 * dW_0z) + (d_c_0 * W_0)
+    g_x = c_0 * W_0X
+    g_y = c_0 * W_0Y
+    g_z = (c_0 * W_0Z) # + (d_c_0 * W_0)  - zakomentované, Bucha to vo výpočte odstránil - opýtať sa prečo (asi kvôli sum^-1)
+
+    g = np.array([g_x,g_y,g_z])   # vloženie do vektora (tuple)
 
     # Tenzor - T ------
 
-
+    U_0XX = lambda X, Y, Z, func_val_0: \
+                -func_val_0[0]
     
+    U_0XY = lambda X, Y, Z, func_val_0: \
+                func_val_0[5]
+    
+    U_0XZ = lambda X, Y, Z, func_val_0: \
+                func_val_0[4]
+    
+    U_0YY = lambda X, Y, Z, func_val_0: \
+                -func_val_0[1]
+    
+    U_0YZ = lambda X, Y, Z, func_val_0: \
+                func_val_0[3]
+    
+    U_0ZZ = lambda X, Y, Z, func_val_0: \
+                -func_val_0[2]
+    
+    W_0XX = triple_dif(U_0XX,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+    W_0XY = triple_dif(U_0XY,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+    W_0XZ = triple_dif(U_0XZ,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+    W_0YY = triple_dif(U_0YY,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+    W_0YZ = triple_dif(U_0YZ,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+    W_0ZZ = triple_dif(U_0ZZ,X1,Y1,Z1,X2,Y2,Z2,homogenous=True)
+
+    # Počiatočná hodnota tenzora ----
+
+    G_0XX = c_0 * W_0XX
+    G_0XY = c_0 * W_0XY
+    G_0XZ = (c_0 * W_0XZ) # + (d_c_0 * W_0X)
+    G_0YY = c_0 * W_0YY
+    G_0YZ = (c_0 * W_0YZ) # + (d_c_0 * W_0Y)
+    G_0ZZ = (c_0 * W_0ZZ) # + (2 * d_c_0 * W_0Z) + (dd_c_0 * W_0) ten istý dôvod zakomentovania
+
+    G     = np.array([[G_0XX, G_0XY, G_0XZ],    # vloženie do matice
+                      [0    , G_0YY, G_0YZ],
+                      [0    , 0    , G_0ZZ]])
 
     # ---------- Nehomogénna prizma ------------- # 
 
     # :) 
 
-    # -- Posledné úpravy --
+     #   U_1 = lambda X, Y, Z, func_val_1: \
+     #   -((Z**(1+2) * func_val_1[5]) / (1 + 2)) + ((Z**(1+1) * ((Y * func_val_1[2]) + (X * func_val_1[1]))) / (1 + 1)) \
+     #   - (((Y * func_val_1[4]) + (X * func_val_1[3])) / ((1 + 1) * (1 + 2)))
+     # 
+     #   W_1 = triple_dif(U_1,X1,Y1,Z1,X2,Y2,Z2,N,homogenous=False)        
 
-    g = [g_x,g_y,g_z]   # vloženie do vektora (tuple)
-
-    return V, g
+    return V, g, G
 
 
 
--- function/testing.py
+++ function/testing.py
@@ -2,13 +2,41 @@ import numpy as np
 from fukushima import *
 from tripledif import *
 
-
-coor_prism = [1,2,3,4,5,6]
-coor_point = [20,30,40]
+                # X      # Y    # Z
+coor_prism = [-0.5,0.5, -1,1, -1.5,1.5] # !!!
+coor_point = [1.5,3.0,4.5]
 rho = [1]
 
+# x1(0,0,0)
+# x2()
 
-V,g = Fukushima(coor_prism,coor_point,rho)
+
+V,g,G = Fukushima(coor_prism,coor_point,rho)
 
 print("potencial =\n",V)
 print("\nzrychlenie = \n",g)
+print("\ntenzor =\n",G)
+
+# VSTUPY 
+
+# Newtonova gravitacna konstanta: 6.67430e-11 m**3 * kg*-1 * s**-2
+# Rozmery prizmy (x, y, z): 1.0, 2.0, 3.0
+# Hustota: 1.0 kg * m**-3
+# Vypoctovy bod (xp, yp, zp): 1.5, 3.0, 4.5
+
+
+# VYSTUPY 
+# v     7.2025779027446533e-11 m**2 * s**-2 # potencial
+
+# vx   -3.6702626407484486e-12 m * s**-2 # zrychlenia
+# vy   -7.1486037410563400e-12 m * s**-2
+# vz   -1.0322950752029843e-11 m * s**-2
+
+# vxx  -1.8489526045069804e-12 s**-2 # tenzor
+# vxy   1.1425556381054625e-12 s**-2
+# vxz   1.6148227073209125e-12 s**-2
+
+# vyy  -2.0256052043887284e-13 s**-2
+# vyz   3.0899526561096312e-12 s**-2
+
+# vzz   2.0515131249458494e-12 s**-2
